9bc6770b18d649a3d2c1a7bff80d34e45a0f07cd
Raghav Bhandari <raghavxb@gmail.com>
2017-08-04 01:54:59 +0530

    Update 66. Plus One.cpp

--- Easy/66. Plus One.cpp
+++ Easy/66. Plus One.cpp
@@ -22,3 +22,23 @@ public:
         return digits;
         }
 };
+
+
+A more efficient solution in C++:
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        bool carry = true;
+        
+        for(int i=digits.size()-1; i >= 0 && carry; i--) {
+            carry = (++digits[i]%=10) == 0;
+        }
+
+        if(carry) {
+            digits.insert(digits.begin(), 1);
+        }
+    
+        return digits;
+    }
+};
