69235dfd01bc30da6e266136080b3294ef373926
Raghav Bhandari <raghavxb@gmail.com>
2017-08-18 23:59:28 +0530

    Create 500. Keyboard Row.py

---
+++ Easy/String/500. Keyboard Row.py
@@ -0,0 +1,25 @@
+Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+
+
+American keyboard
+
+
+Example 1:
+Input: ["Hello", "Alaska", "Dad", "Peace"]
+Output: ["Alaska", "Dad"]
+Note:
+You may use one character in the keyboard more than once.
+You may assume the input string will only contain letters of alphabet.
+
+
+
+
+class Solution(object):
+    def findWords(self, words):
+        line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+        ret = []
+        for word in words:
+            w = set(word.lower())
+            if w.issubset(line1) or w.issubset(line2) or w.issubset(line3):
+                ret.append(word)
+        return ret
