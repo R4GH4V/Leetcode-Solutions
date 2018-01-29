39f0f0f092d3cf4208e799433538c8dc20fba945
Raghav Bhandari <raghavxb@gmail.com>
2018-01-28 23:05:48 -0800

    Create 384. Shuffle an Array.java

---
+++ Medium/Array/384. Shuffle an Array.java
@@ -0,0 +1,60 @@
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+
+
+
+class Solution {
+    private int[] nums;
+    private Random random;
+
+    public Solution(int[] nums) {      
+        
+        this.nums=nums;        
+        random=new Random();
+            
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;        
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if(nums==null) return null;
+        int[] a = nums.clone();
+        for(int i=1;i<nums.length;i++)
+        {
+            int j=random.nextInt(i+1);
+            swap(a,i,j);
+        }
+        return a;
+    }
+    
+    public void swap(int[] arr,int i,int j){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
