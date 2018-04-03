fce74ee5d65c9ad56d9eb96c46a6fe277ba761a2
Raghav Bhandari <raghavxb@gmail.com>
2018-04-02 23:28:32 -0700

    Create 118. Pascal's Triangle.java

---
+++ Easy/Array/118. Pascal's Triangle.java
@@ -0,0 +1,37 @@
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle=new ArrayList<List<Integer>>();
+        if(numRows<=0)
+        {
+            return triangle;
+        }
+        for(int i=0;i<numRows;i++)
+        {
+            List<Integer> row=new ArrayList<Integer>();
+            for(int j=0;j<i+1;j++)
+            {
+                if(j==0 || j==i)
+                    row.add(1);
+                else
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
